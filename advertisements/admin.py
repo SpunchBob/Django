2ecb06d3e479ffbb55c22200a620b0e16c9626fb
SpunchBob <aleksandrprokudin2007@gmail.com>
2023-08-18 15:33:57 +0300

    дз

--- advertisements/admin.py
+++ advertisements/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Advertisement
 
-# Register your models here.
+class AdvertismentAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "auction", "price", "created_at", "created_day"]
+    list_filter = ["auction", "created_at"]
+    search_fields = ["title"]
+    actions = ["mark_auction_as_true", "mark_auction_as_false"]
+    fieldsets = (
+        ('Общая информация' , {
+            "fields":("title", "description"),
+            "classes":["collapse"]
+        }),
+        ("Финансы", {
+            "fields": ("price", "auction"),
+            "classes":["collapse"]
+        })
+    )
+
+    @admin.action(description="Добавить возможность")
+    def mark_auction_as_true(self, request, queryset):
+        queryset.update(auction=True)
+
+    @admin.action(description="убрать возможность")
+    def mark_auction_as_false(self, request, queryset):
+        queryset.update(auction=False)
+
+
+admin.site.register(Advertisement, AdvertismentAdmin)
--- advertisements/models.py
+++ advertisements/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+
+class Advertisement(models.Model):
+    title = models.CharField(max_length=80)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    auction = models.BooleanField(help_text="Отметьте, если торг уместен")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    @admin.display
+    def created_day(self):
+        from django.utils import timezone    
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M")
+            return f"Сегодня в {created_time}"
+        return self.created_at
+
+class Meta:
+    db_table = "advertisements"
+    verbose_name = "объявление"
+    verbose_name_plural = "объявления"
